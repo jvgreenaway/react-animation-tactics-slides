cfbf07a89437731d3493ccd03f8f238ebea10604
James Greenaway <james@greenaway.io>
2014-11-18 17:36:48 +0000

    Modal CSS Group.

---
+++ scripts/components/modal-css-group.js.jsx
@@ -0,0 +1,47 @@
+var React = require('react/addons'),
+    CSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+
+module.exports = React.createClass({  
+  displayName: 'ModalCSSGroup',
+
+  getInitialState: function() {
+    return {
+      visible: false
+    };
+  },
+
+  show: function () {
+    this.setState({visible: true});
+  },
+  hide: function () {
+    this.setState({visible: false});
+  },
+
+
+  render: function () {    
+    var modalElements = [];
+    if (this.state.visible) {
+      modalElements = [
+        <div key='modal' className='modal'>
+          <button 
+            className='modal__btn' 
+            onClick={this.hide}
+          >Close</button>
+        </div>,
+        <div key='shade' className='shade' />
+      ];
+    }
+
+    return (
+      <div>
+        <CSSTransitionGroup transitionName='tr' className='list'>
+          { modalElements }
+        </CSSTransitionGroup>
+
+        <button onClick={this.show}>Show</button>
+      </div>
+    );
+  }
+});
+  
--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,8 @@ var React = require('react/addons'),
     domready = require('domready');
 
 var FadeInGroup = React.createFactory(require('./components/fade-in-group')),
-    TranslateInGroup = React.createFactory(require('./components/translate-in-group'));
+    TranslateInGroup = React.createFactory(require('./components/translate-in-group')),
+    ModalCSSGroup = React.createFactory(require('./components/modal-css-group'));
 
 
 domready(function() {
@@ -15,6 +16,11 @@ domready(function() {
   React.render(
     TranslateInGroup(), 
     document.getElementById('translate-in-group-demo')
+  );  
+
+  React.render(
+    ModalCSSGroup(), 
+    document.getElementById('modal-css-group-demo')
   );  
 
 });
