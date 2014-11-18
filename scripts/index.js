d75423e93506cbf1b4fcdcecc6e66fd79d9bd35a
James Greenaway <james@greenaway.io>
2014-11-18 17:11:46 +0000

    Translate in demo.

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
 var React = require('react/addons'),
     domready = require('domready');
 
-var FadeInGroup = React.createFactory(require('./components/fade-in-group'));
+var FadeInGroup = React.createFactory(require('./components/fade-in-group')),
+    TranslateInGroup = React.createFactory(require('./components/translate-in-group'));
 
 
 domready(function() {
@@ -9,6 +10,11 @@ domready(function() {
   React.render(
     FadeInGroup(), 
     document.getElementById('fade-in-group-demo')
+  );  
+
+  React.render(
+    TranslateInGroup(), 
+    document.getElementById('translate-in-group-demo')
   );  
 
 });
